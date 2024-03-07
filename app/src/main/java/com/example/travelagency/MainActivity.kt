2cc09d7ceb70773a40752d54e891cac7031d0e8b
AnyaGimpel <annagimpel70@gmail.com>
2024-03-07 15:51:10 +0300

    Commit fragments and navBar

--- app/src/main/java/com/example/travelagency/MainActivity.kt
+++ app/src/main/java/com/example/travelagency/MainActivity.kt
@@ -2,10 +2,46 @@ package com.example.travelagency
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.travelagency.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(HomeFragment())
+
+        val fragmentToOpen = intent.getStringExtra("fragmentToOpen")
+        if (fragmentToOpen != null) {
+            replaceFragment(ProfileFragment())
+            binding.bottomNavigationView.selectedItemId = R.id.profile
+        }
+
+        binding.bottomNavigationView.setOnItemSelectedListener{
+
+            when(it.itemId){
+
+                R.id.home -> replaceFragment(HomeFragment())
+                R.id.cart -> replaceFragment(CartFragment())
+                R.id.profile -> replaceFragment(ProfileFragment())
+
+                else ->{
+
+                }
+
+            }
+            true
+        }
+
+    }
+
+    private fun replaceFragment(fragment : Fragment){
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction  = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout,fragment)
+        fragmentTransaction.commit()
     }
 }
